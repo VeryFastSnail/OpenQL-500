598edb131f9112f580fbb407e05c1265c2d408ef
VeryFastSnail <32437815+VeryFastSnail@users.noreply.github.com>
2018-06-26 14:00:34 +0300

    Update atiriny.ino

--- Code/atiriny.ino
+++ Code/atiriny.ino
@@ -1,13 +1,13 @@
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // ATTiny13A pin 0 PWM 
   pinMode(0, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(0, HIGH);   // Sensor HIGH
-  delay(250);                       // wait for a bit
+  delay(250);                       //wait for a bit
   digitalWrite(0, LOW);    // Sensor LOW
-  delay(250);                       // wait for a wait for abit
+  delay(250);                       //wait for a bit
 }
